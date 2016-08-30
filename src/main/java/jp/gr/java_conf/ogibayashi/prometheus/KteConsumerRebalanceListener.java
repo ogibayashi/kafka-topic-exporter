bc45400b8d7c6c40e4fe86ca05f31148b17ab126
Hironori Ogibayashi <hironori.ogibayashi@linecorp.com>
2016-08-30 13:09:08 +0900

    Added logging when assinged/revoked partition

--- src/main/java/jp/gr/java_conf/ogibayashi/prometheus/KteConsumerRebalanceListener.java
+++ src/main/java/jp/gr/java_conf/ogibayashi/prometheus/KteConsumerRebalanceListener.java
@@ -1,11 +1,15 @@
 package jp.gr.java_conf.ogibayashi.prometheus;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.clients.consumer.ConsumerRebalanceListener;
 import org.apache.kafka.clients.consumer.Consumer;
 import java.util.Collection;
 
 public class KteConsumerRebalanceListener implements ConsumerRebalanceListener {
+    private static final Logger LOG = LoggerFactory.getLogger(KteConsumerRebalanceListener.class);
     private Consumer<String,String> consumer;
 
     public KteConsumerRebalanceListener(Consumer<String,String> consumer) {
@@ -13,9 +17,15 @@ public class KteConsumerRebalanceListener implements ConsumerRebalanceListener {
     }
 
     public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
+        for (TopicPartition t: partitions) {
+            LOG.info("Revoked partition. topic: {}, partition: {}", t.topic(), t.partition());
+        }
     }
 
     public void onPartitionsAssigned(Collection<TopicPartition> partitions) {
+        for (TopicPartition t: partitions) {
+            LOG.info("Assigned partition. topic: {}, partition: {}", t.topic(), t.partition());
+        }
     }
 }
  
